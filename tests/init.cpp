452913824ee0a0c5338d3e9232b1be97bc0b406e
Bozey98 <dmusatov98@gmail.com>
2017-03-27 12:48:19 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -16,6 +16,74 @@ SCENARIO("complex init  parametrs", "[ wp]") {
 SCENARIO("complex sum", "[sum]") {
 	complex a(1,1),b(2,2),c(3,3);
 	REQUIRE(a.add(b) ==c);
+}
+
+SCENARIO("complex sub", "[sub]") {
+	complex a(3,3),b(2,2),c(1,1);
+	REQUIRE(a.sub(b) == c);
+}
+
+SCENARIO("Proizv", "[Pro]") {
+	complex a(1,1),b;
+	int k = 4;
+	b = a.Proizv(k);
+	REQUIRE(b.deist() == 4);
+	REQUIRE(b.mnim() == 4);
+}
+
+SCENARIO ("Chast", "[Cha]") {
+	complex a(4,4),b;
+	int k = 4;
+	b = a.Chast(k);
+	REQUIRE(b.deist() == 1);
+	REQUIRE(b.mnim() == 1);
+}
+
+SCENARIO("complex sum", "[sum]") {
+	complex a(1,1),b(2,2),c(3,3);
+	REQUIRE(a.add(b) ==c);
+}
+
+SCENARIO("operator *", "[op*]") {
+	complex a(1,1),b(2,2),c(-3,4);
+	c = a*b;
+	REQUIRE(a==c);
+}
+
+SCENARIO("operator /", "[op/]") {
+	complex a(4,7),b(3,1),c(0.62,0.34);
+	c = a/b;
+	REQUIRE(a==c);
+}
+
+SCENARIO("operator +=", "[op+=]") {
+	complex a(3,3),b(2,2),c(1,1);
+	a-=b;
+	REQUIRE(a==c);
+}
+
+SCENARIO("operator ==", "[op==]") {
+	complex a(1,1),b(1,1);
+	REQUIRE(a==b);
+}
+
+SCENARIO("operator *=", "[op*]") {
+	complex a(1,1),b(2,2),c(-3,4);
+	a=*b;
+	REQUIRE(a==c);
+}
+
+SCENARIO("operator /=", "[op/=]") {
+	complex a(4,7),b(3,1),c(0.62,0.34);
+	a/=b;
+	REQUIRE(a==c);
+}
+
+SCENARIO("operator =", "[op=]") {
+	complex a(1,1),b(2,2);
+	a = b;
+	REQUIRE(a==b);
+}
 
 
 
