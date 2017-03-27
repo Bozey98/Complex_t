c91ab2745bdd50f7c5ad6078cff8545e8b3185c6
Bozey98 <dmusatov98@gmail.com>
2017-03-27 11:51:47 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -2,5 +2,22 @@
 #include <catch.hpp>
 
 SCENARIO("complex init without parametrs", "[init wp]") {
+	complex h;
+	REQUIRE(h.deist() == 0);
+	REQUIRE(h.mnim() == 0);
+}
+
+SCENARIO("complex init  parametrs", "[ wp]") {
+	complex h(3,3);
+	REQUIRE(h.deist() == 3);
+	REQUIRE(h.mnim() == 3);
+}
+
+SCENARIO("complex sum", "[sum]") {
+	complex a(1,1),b(2,2),c(3,3);
+	REQUIRE(a.add(b) ==c);
+
+
+
 	
 	
